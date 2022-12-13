49647c75300f04177c440b35b317d7b04499cf23
Eray Çakır <99022088+eraycakiir@users.noreply.github.com>
2022-12-13 12:53:11 +0300

    we connected to the database for the first time

---
+++ Database-First/DataBaseFirst/DataBaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-TP2LRU1\\SQLEXPRESS;Initial Catalog=EfCore-DatabaseFirst;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+    }
+}
--- Database-First/DataBaseFirst/DataBaseFirst/Program.cs
+++ Database-First/DataBaseFirst/DataBaseFirst/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DataBaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+}
